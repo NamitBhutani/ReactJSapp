f97822c590817314e2373ef809d1d0d712f9d063
Namit Bhutani <namit.bhut@gmail.com>
2022-11-23 09:26:28 +0530

    Added employeeId EDIT functionality :D

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 function App() {
   const [role, setRole] = useState("NIL");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Bully Maguire",
       role: "UNfriendly neighbourhood Spider-Man",
       img: "https://i1.sndcdn.com/artworks-g0uZiBmAbb4Gy4U2-qoCInQ-t500x500.jpg",
     },
     {
+      id: 2,
       name: "Tom Holland",
       role: "baby Spider-Man",
       img: "https://i.imgflip.com/2bffc8.jpg?a463080",
     },
   ]);
+  function updateEmployee(id, newName, newRole) {
+    console.log("hello from app.js");
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
   return (
     <div className="App">
       <input
@@ -28,10 +39,11 @@ function App() {
       {employees.map((employee) => {
         return (
           <Employee
-            key={uuidv4()}
+            id={employee.id}
             name={employee.name}
             role={employee.role}
             img={employee.img}
+            updateEmployee={updateEmployee}
           />
         );
       })}
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee() {
+function EditEmployee(props) {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+      <button
+        onClick={handleShow}
+        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      >
         Update
       </button>
 
@@ -21,17 +26,67 @@ function EditEmployee() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("hello from editemployee");
+              props.updateEmployee(props.id, name, role);
+            }}
+            id="EmployeeInput"
+            class="w-full max-w-sm"
+          >
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="inline-full-name"
+                >
+                  Full Name
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-full-name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="inline-full-name"
+                >
+                  Role
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-full-name"
+                  type="text"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <button form="EmployeeInput">Update</button>
         </Modal.Footer>
       </Modal>
     </>
