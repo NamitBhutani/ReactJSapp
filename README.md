aa87feac348f1ade39e5705d11cefdc05cbf0f52
Namit Bhutani <namit.bhut@gmail.com>
2022-11-30 13:55:52 +0530

    Updated readme and finalised ghpages website

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+Link- https://namitbhutani.github.io/ReactJSapp/
+
 This is a project I made for learning React and with other frontend and backend tech :)
 Basically its a employee detail editor till now, might get changed in the final version lel
--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import "./App.css";
-//import Employee from "./components/Employee";
-//import { useState } from "react";
-//import AddEmployee from "./components/AddEmployee";
-import Employees from "./pages/Employees";
-import Team from "./pages/Team";
-import Header from "./components/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return <></>;
 }
