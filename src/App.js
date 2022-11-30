4ee4920da4e758883e174371d3f4697281e5cc6e
Namit Bhutani <namit.bhut@gmail.com>
2022-11-30 11:19:33 +0530

    added hasrouter in app.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import "./App.css";
 import Employees from "./pages/Employees";
 import Team from "./pages/Team";
 import Header from "./components/Header";
-import { Switch, BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Header>
           <Routes>
             <Route path="/employees" element={<Employees />} />
             <Route path="/team" element={<Team />} />
           </Routes>
         </Header>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
